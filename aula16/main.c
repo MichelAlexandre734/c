7e590e5857ef8e931723a5bbf86841464b55130a
MICHEL ALEXANDRE DE SOUZA SILVA <michelalexandre972@gmail.com>
2026-06-09 00:15:07 -0300

    mudei o site

--- aula16/main.c
+++ aula16/main.c
@@ -4,8 +4,8 @@
 int main()
 {
     float x=1.0;
-     printf("tamanho de um float na memoria:%dbytes\n",sizeof x);
-    printf("tamanho em memoria de  um int : %d bytes\n",sizeof(int));
-    // 4  x 8 = 32 bits
+    printf("tamanho de um float  na memoria: %dbytes\n",sizeof x);
+   printf("tamanho em memoria de um int:%dbytes\n",sizeof(int));
+   // 4 x 8=32bits
     return 0;
 }
